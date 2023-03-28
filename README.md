85083b425cb098555e148dc2075559252e62d3b4
Bruno Shimura <bruno_shimura@hotmail.com>
2023-03-28 10:03:51 -0300

    start-project

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# drawings-classifier
+# AI Custom drawings classifier with python
+A Python application which uses machine learning classification algorithms to classify drawing of the user.
+ 
+[NeuralNine](https://www.youtube.com/watch?v=cwwqDn57LUk)  
---
+++ main.py
@@ -0,0 +1,46 @@
+import pickle
+import os.path
+
+import tkinter.messagebox
+from tkinter import *
+from tkinter import simpledialog
+
+import PIL
+import cv2 as cv
+import numpy as np
+
+from sklearn.svm import LinearSVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+class DrawingClassifier:
+    
+    def __init__(self):
+        self.class1, self.class2, self.class3 = None, None, None
+        self.class1_counter, self.class2_counter, self.class3_counter = None, None, None
+        self.clf = None
+        self.proj_name = None
+        self.root = None
+        self.image1 = None
+
+        self.status_label = None
+        self.canvas = None
+        self.draw = None
+
+        self.brush_width = 15
+
+        self.classes_prompt()
+        self.init_gui()
+
+    def classes_prompt(self):
+        msg = Tk()
+        msg.withdraw()
+
+        self.proj_name = simpledialog.askstrings("Project Name", "Please enter your project name down below!", parent=msg)
+        if os.path.exists(self.proj_name):
+            with open(f"{self.proj_name}/{self.proj_name}_data.pickle", "rb") as f:
+                data = pickle.load(f)    
+    def init_gui(self):
+        pass
